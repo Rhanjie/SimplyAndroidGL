194328e7c83d51fee5c9a91cfe0ebc178bfeade4
RhAnjiE <irhanjie@gmail.com>
2019-03-28 02:44:32 +0100

    Added MVP Matrix

--- app/src/main/java/com/rhanjie/me/opengltest/drawable/MyGLRenderer.kt
+++ app/src/main/java/com/rhanjie/me/opengltest/drawable/MyGLRenderer.kt
@@ -2,6 +2,7 @@ package com.rhanjie.me.opengltest.drawable
 
 import android.opengl.GLES31
 import android.opengl.GLSurfaceView
+import android.opengl.Matrix
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -11,6 +12,10 @@ class MyGLRenderer : GLSurfaceView.Renderer {
 
     private lateinit var triangleCoords: FloatArray
     private lateinit var squareCoords: FloatArray
+
+    private val vPMatrix = FloatArray(16)
+    private val projectionMatrix = FloatArray(16)
+    private val viewMatrix = FloatArray(16)
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
         GLES31.glClearColor(0.2f, 0.5f, 1.0f, 1.0f)
@@ -29,12 +34,20 @@ class MyGLRenderer : GLSurfaceView.Renderer {
     override fun onDrawFrame(gl: GL10?) {
         GLES31.glClear(GLES31.GL_COLOR_BUFFER_BIT)
 
-        triangle.draw()
+        Matrix.setLookAtM(viewMatrix, 0, 0f, 0f, -3f, 0f, 0f, 0f, 0f, 1f, 0f)
+        Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
+
+        triangle.draw(vPMatrix)
+
+        //TODO: Finish square class
         //square.draw();
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
         GLES31.glViewport(0, 0, width, height)
+
+        val ratio: Float = width.toFloat() / height.toFloat()
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 7f)
     }
 
     companion object {
--- app/src/main/java/com/rhanjie/me/opengltest/drawable/Triangle.kt
+++ app/src/main/java/com/rhanjie/me/opengltest/drawable/Triangle.kt
@@ -4,12 +4,14 @@ import android.opengl.GLES31
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.FloatBuffer
+import kotlin.random.Random
 
 class Triangle(var coords: FloatArray) {
     private val vertexShaderCode = "" +
+            "uniform mat4 uMVPMatrix;" +
             "attribute vec4 vPosition;" +
             "void main() {" +
-            "   gl_Position = vPosition;" +
+            "   gl_Position = uMVPMatrix * vPosition;" +
             "}"
 
     private val fragmentShaderCode = "" +
@@ -22,17 +24,21 @@ class Triangle(var coords: FloatArray) {
 
     val vertices = 3
     val coordsPerVertex = 3
-    val color = floatArrayOf(0.5f, 1.0f, 0.0f, 1.0f)
+    var color: FloatArray
+        private set
 
     private var program: Int
 
     private var positionHandle: Int = 0
     private var colorHandle: Int = 0
+    private var vPMatrixHandle: Int = 0
 
     private val vertexCount: Int = (vertices * coordsPerVertex) / coordsPerVertex
     private val vertexStride: Int = coordsPerVertex * 4
 
     init {
+        color = floatArrayOf(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1.0f)
+
         val vertexShader: Int = MyGLRenderer.loadShaderFromMemory(GLES31.GL_VERTEX_SHADER, vertexShaderCode)
         val fragmentShader: Int = MyGLRenderer.loadShaderFromMemory(GLES31.GL_FRAGMENT_SHADER, fragmentShaderCode)
 
@@ -43,20 +49,24 @@ class Triangle(var coords: FloatArray) {
         }
     }
 
-    fun draw() {
+    fun draw(mvpMatrix: FloatArray) {
         GLES31.glUseProgram(program)
-        positionHandle = GLES31.glGetAttribLocation(program, "vPosition").also {
-            GLES31.glEnableVertexAttribArray(it)
-            GLES31.glVertexAttribPointer(it, coordsPerVertex, GLES31.GL_FLOAT, false, vertexStride, vertexBuffer)
 
-            colorHandle = GLES31.glGetAttribLocation(program, "vColor").also {
+        positionHandle = GLES31.glGetAttribLocation(program, "vPosition").also {
+            GLES31.glEnableVertexAttribArray(positionHandle)
+            GLES31.glVertexAttribPointer(positionHandle, coordsPerVertex, GLES31.GL_FLOAT, false, vertexStride, vertexBuffer)
+
+            colorHandle = GLES31.glGetUniformLocation(program, "vColor").also {
                 GLES31.glUniform4fv(colorHandle, 1, color, 0)
             }
 
-            GLES31.glDrawArrays(GLES31.GL_TRIANGLES, 0, vertexCount)
-            GLES31.glDisableVertexAttribArray(it)
-        }
+            vPMatrixHandle = GLES31.glGetUniformLocation(program, "uMVPMatrix").also {
+                GLES31.glUniformMatrix4fv(vPMatrixHandle, 1, false, mvpMatrix, 0)
+            }
 
+            GLES31.glDrawArrays(GLES31.GL_TRIANGLES, 0, vertexCount)
+            GLES31.glDisableVertexAttribArray(positionHandle)
+        }
     }
 
     private var vertexBuffer: FloatBuffer = ByteBuffer.allocateDirect((vertices * coordsPerVertex) * 4).run {
